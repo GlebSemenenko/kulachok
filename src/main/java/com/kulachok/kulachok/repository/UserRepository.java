b5ddf326abe3533a24b61b9fbc55ebdd53bd55ad
KOTEDO <tray202_99@mail.ru>
2025-02-07 16:16:47 +0300

    Редактирование процессов создания и логирования сущностей

--- src/main/java/com/kulachok/kulachok/repository/UserRepository.java
+++ src/main/java/com/kulachok/kulachok/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.kulachok.kulachok.repository;
 
+import com.kulachok.kulachok.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
 }
